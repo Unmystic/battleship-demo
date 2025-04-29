f5ce3d6f498a42b0432366714c5628e09da0d3a8
Unmystic <unmystic7@gmail.com>
2025-04-29 10:19:22 +0200

    display basic boards design on a page

--- battleship.js
+++ battleship.js
@@ -1,16 +1,32 @@
 import { Player } from "./player.js";
 import { Ship } from "./ship.js";
 
-const gameCont = document.querySelector(".gameCont");
+const gameCont = document.querySelectorAll(".gameCont");
 
-function createBoard(num = 100) {
-    const numCells = num;
-
-    for (let i = 0; i < numCells; i++) {
+function createBoard(board, side = "left") {
+    const gameCont =
+        side === "left"
+            ? document.querySelector(".leftBoard")
+            : document.querySelector(".rightBoard");
+    const grid = board.grid.flat();
+    grid.forEach((c) => {
         const cell = document.createElement("div");
-        console.log(i);
         gameCont.appendChild(cell);
-    }
+        if (c === 1) {
+            console.log(cell);
+            cell.style.border = "0.75mm ridge #968ea4";
+
+            cell.classList.add("ridge");
+        }
+    });
 }
 
-createBoard();
+function setPlayers() {
+    const player1 = new Player(true);
+    const player2 = new Player(false);
+    player1.board.grid[0] = new Array(10).fill(1);
+    createBoard(player1.board, "left");
+    createBoard(player2.board, "right");
+}
+
+setPlayers();
